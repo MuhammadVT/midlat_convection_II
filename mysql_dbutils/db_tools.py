ad4a1ca72e4db16be2587e750601eceecf1369f1
MuhammadVT <mamatripik@gmail.com>
2017-07-20 15:36:35 -0400

    add db_tools.py

---
+++ mysql_dbutils/db_tools.py
@@ -0,0 +1,34 @@
+def create_db(db_name, config_filename="../mysql_dbconfig_files/config.ini",
+              section="midlat"):
+    """ Creates a MySQL table.
+
+    Parameters
+    ----------
+    config_filename: str
+	name and path of the configuration file
+    section: str, default to mysql
+	section of database configuration
+  
+    Returns
+    -------
+    MySQLConnection
+        MySQL connection to the newly created db.
+
+    """
+    
+    from mysql.connector import MySQLConnection
+    from db_config import db_config
+
+    # read the db config info
+    config =  db_config(config_filename=config_filename, section=section)
+    config_info = config.read_db_config()
+
+    # make db connection
+    conn = MySQLConnection(**config_info)
+
+    # create a db
+    command = "CREATE DATABASE IF NOT EXISTS {db}".format(db=db_name) 
+    conn.cursor().execute(command)
+
+    return conn
+
