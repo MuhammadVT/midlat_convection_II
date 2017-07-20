12c22d13fda93b93b95e4cd1265579fe6f3cc9e0
MuhammadVT <mamatripik@gmail.com>
2017-07-20 10:17:30 -0400

    do some cleaning

--- move_to_db/readfile_to_db.py
+++ move_to_db/readfile_to_db.py
@@ -8,14 +8,13 @@ import pdb
 
 class read_file_to_db(object):
     """ A class that holds the boxcar median filtered data after reading them from a file.
-    It writes the data into a sqlite db using its move_to_db method."""
+    It writes the data into a MySQL db using its move_to_db method."""
 
     def __init__(self, rad, ctr_date, ftype="fitacf", params=["velocity"], ffname=None):
 
         """ 
         Parameters
         ----------
-
         rad : str
             Three-letter code for a rad
         ctr_date : datetime.datetime
@@ -58,7 +57,7 @@ class read_file_to_db(object):
         etm = self.ctr_date + dt.timedelta(days=1) + dt.timedelta(minutes=2)
 
         # read data from file 
-        # Note: data_from_db argument has to be False
+        # Note: data_from_db and plotrti arguments have to be False
         self.data = read_file(self.ffname, rad, stm, etm, params,
                               ftype=self.ftype, data_from_db=False,
                               plotrti=False)
@@ -97,7 +96,7 @@ class read_file_to_db(object):
 
         Parameters
         ----------
-        conn : sqlite3.connect
+        conn : MySQLdb.connect
 
         Returns
         -------
@@ -136,32 +135,47 @@ class read_file_to_db(object):
         # commit the change, once at one day of data points
         conn.commit()
 
+def worker(dbName, baseLocation, rad, bmnum, ctr_date, 
+           ftype, params, ffname):
+    """ A worker function used for multiprocessing.
+    """
+
+    import mysql.connector import MySQLConnection, Error
+    import datetime as dt
+    # make a db connection
+    conn = sqlite3.connect(baseLocation + dbName)
+
+    # collect the data 
+    t1 = dt.datetime.now()
+    print "creating an object for " + rad + " for " + str(ctr_date)
+    rf = read_file_to_db(rad, ctr_date, ftype=ftype, params=params, ffname=ffname)
+    print "created an object for " + rad + " for " + str(ctr_date)
+    if rf.data is not None:
+
+        # move data to db
+        rf.move_to_db(conn)
+        print ("object has been moved to db")
+
+    t2 = dt.datetime.now()
+    print ("creating and moving object to the db took " +\
+	    str((t2-t1).total_seconds() / 60.)) + " mins\n"
+
+    # close db connection
+    conn.close()
+
 def main():
     """ Call the functions above. Acts as an example code.
-    Multiprocessing has been implemented to do parallel computing. The unit process is for """
+    Multiprocessing has been implemented to do parallel computing.
+    The unit process is for reading a day worth of data for a given radar"""
     
     import datetime as dt
     import sqlite3
-    from month_to_season import get_season_by_month
     import multiprocessing as mp
 
     # input parameters
-    #season = "winter"
-    #season = "summer"
-    season = "equinox"
-
-    # set the time interval for each season
-    # NOTE: The code processes two years (full years specified by year_1 and year_2) of 
-    #data at a time.
-    year_0 = 2014; year_1 = 2015; year_2 = 2016; year_3 = 2017
-    #year_0 = 2010; year_1 = 2011; year_2 = 2012; year_3 = 2013
 
     # run the code for the following radars in parallel
     #rad_list = ["bks", "wal", "fhe", "fhw", "cve", "cvw"]
-    #rad_list = ["bks", "wal", "fhe"]
-    #rad_list = ["fhw", "cve", "cvw"]
-    rad_list = ["tig", "unw"]
-    #rad_list = ["unw"]
     channel = None
     params=['velocity']
     ftype = "fitacf"
@@ -169,59 +183,47 @@ def main():
     ffname = None
 
     # make a db connection
-#    season =  get_season_by_month(sdate.month)
-    baseLocation = "../data/sqlite3/" + season + "/"
-
-    # define the staring and ending dates for a given season
-    # NOTE : winter (Nov - Feb), summer (May - Sep), equinox (Mar - Apr, Oct - Nov)
-    if season == "winter":
-        stms = [dt.datetime(year_0,12,31), dt.datetime(year_1,10,31), dt.datetime(year_2,10,31)]
-        etms = [dt.datetime(year_1,3,1), dt.datetime(year_2,3,1), dt.datetime(year_3,1,1)]
-
-    if season == "summer":
-        stms = [dt.datetime(year_1,4,30), dt.datetime(year_2,4,30)]
-        etms = [dt.datetime(year_1,9,1), dt.datetime(year_2,9,1)]
-
-    if season == "equinox":
-        stms = [dt.datetime(year_1,2,28), dt.datetime(year_1,8,31), dt.datetime(year_2,2,29), dt.datetime(year_2,8,31)]
-        etms = [dt.datetime(year_1,5,1), dt.datetime(year_1,11,1), dt.datetime(year_2,5,1), dt.datetime(year_2,11,1)]
-
-    # loop through the time intervals
-    for dd in range(len(stms)):
-        sdate = stms[dd]
-        edate = etms[dd]
-        num_days = (edate - sdate).days + 1
-        dtm_range = [sdate + dt.timedelta(days=i) for i in xrange(num_days)]
-
-        # loop through the radars
-        for rad in rad_list:
+    baseLocation = "../data/sqlite3/"
 
             # make a db connection
             dbName = rad + "_" + ftype + ".sqlite"
             conn = sqlite3.connect(baseLocation + dbName)
 
-            # loop through the days within a time interval:
-            for ctr_date in dtm_range:
-
-                # collect the data 
-                t1 = dt.datetime.now()
-                print "creating an object for " + rad + " for " + str(ctr_date)
-                rf = read_file_to_db(rad, ctr_date, ftype=ftype, params=params, ffname=ffname)
-                print "created an object for " + rad + " for " + str(ctr_date)
-                if rf.data is not None:
-
-                    # move data to db
-                    rf.move_to_db(conn)
-                    print ("object has been moved to db")
-
-                t2 = dt.datetime.now()
-                print ("creating and moving object to the db took " + str((t2-t1).total_seconds() / 60.)) + " mins\n"
-
             # close db connection
             conn.close()
 
-    return
+    # loop through the dates in all_dates 
+    for ctr_date in all_dates:
 
+        # loop through the radars
+        for rad in rad_list:
+
+            # Store multiprocesses in a list
+            procs = []
+            for rad in [rad]:
+
+                # Skip the date if rad has not data for that date
+                if ctr_date not in rads_dict[rad]['date']:
+                    continue
+
+
+                # Creat a processe
+                idx_tmp = rads_dict[rad]['date'].index(ctr_date)
+                bmnum = rads_dict[rad]['beam'][idx_tmp]
+                dbName = rad + "_" + ftype + ".sqlite"
+                p = mp.Process(target=worker,
+                               args=(dbName, baseLocation, rad, bmnum, ctr_date,
+                                     ftype, params, ffname))
+                procs.append(p)
+
+                # Run the process
+                p.start()
+
+            # Make sure the processes terminate
+            for p in procs:
+                p.join()
+
+    return
 
 if __name__ == '__main__':
     main()
