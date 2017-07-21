136be43968a0485ff708fd19fc189db2b43acd8e
MuhammadVT <mamatripik@gmail.com>
2017-07-21 15:09:28 -0400

    implemented multiprocessing

--- data_preprocessing/readfile_to_db.py
+++ data_preprocessing/readfile_to_db.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on July. 11, 2017
 
 Muhamamd
-'''
+"""
 
 import pdb
 
@@ -37,7 +37,7 @@ class read_file_to_db(object):
 
         import sys
         sys.path.append("../")
-        from dopsearch_py.dopsearch import read_file
+        from classification_of_HF_radar_backscatter.iscat_identifier import read_data
         import datetime as dt
         
         # build the attributes
@@ -58,9 +58,8 @@ class read_file_to_db(object):
 
         # read data from file 
         # Note: data_from_db and plotrti arguments have to be False
-        self.data = read_file(self.ffname, rad, stm, etm, params,
-                              ftype=self.ftype, data_from_db=False,
-                              plotrti=False)
+        self.data = read_data(rad, stm, etm, params, ftype=ftype, ffname=self.ffname,
+                              data_from_db=False, plotrti=False)
 
     def _construct_filename(self, basedir="../data/"):
         """ constructs filename with full path for a file of interest
@@ -92,7 +91,7 @@ class read_file_to_db(object):
         return ffname
 
     def move_to_db(self, conn):
-        """ writes the data into sqlite db
+        """ writes the data into a MySQL db
 
         Parameters
         ----------
@@ -104,46 +103,48 @@ class read_file_to_db(object):
         """
 
         import json 
-        import sqlite3
-
         cur = conn.cursor()
 
         # loop through all the beams
         for bmnum in self.data.keys():
             data_dict = self.data[bmnum]
 
-            # create a table in sqlite db
+            # create a table
             table_name = self.rad + "_bm" + str(bmnum)
             command = "CREATE TABLE IF NOT EXISTS {tb} (\
-                      vel TEXT, rsep REAL, frang REAL, bmazm REAL,\
-                      slist TEXT, gsflg TEXT,\
-                      datetime TIMESTAMP PRIMARY KEY)".format(tb=table_name)
+                      vel TEXT DEFAULT NULL,\
+                      rsep TINYINT(4) DEFAULT NULL,\
+                      frang SMALLINT(4) DEFAULT NULL,\
+                      bmazm FLOAT(7,2) DEFAULT NULL,\
+                      slist TEXT DEFAULT NULL,\
+                      gsflg TEXT DEFAULT NULL,\
+                      datetime DATETIME,\
+                      PRIMARY KEY (datetime))".format(tb=table_name)
             cur.execute(command)
 
             # loop through each scan time, usually 2 minutes,
-            # and write the data into table_name in the sqlite db
+            # and write the data into table_name in db
             for i in xrange(len(data_dict['datetime'])):
-                command = "INSERT OR IGNORE INTO {tb} (vel, rsep, frang, bmazm,\
-                            slist, gsflg, datetime) VALUES (?, ?, ?, ?, ?, ?, ?)"\
-                            .format(tb=table_name)
+                command = "INSERT IGNORE INTO {tb} (vel, rsep, frang, bmazm, " +\
+                          "slist, gsflg, datetime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                command = command.format(tb=table_name)
                 cur.execute(command, (json.dumps(data_dict["vel"][i]), data_dict["rsep"][i],
                             data_dict["frang"][i], data_dict["bmazm"][i],
                             json.dumps(data_dict["slist"][i]),
                             json.dumps(data_dict["gsflg"][i]),\
                             data_dict["datetime"][i]))
 
-        # commit the change, once at one day of data points
+        # commit the change, once at one-day of data points
         conn.commit()
 
-def worker(dbName, baseLocation, rad, bmnum, ctr_date, 
-           ftype, params, ffname):
+        return 
+
+def worker(conn, rad, ctr_date, ftype, params, ffname):
     """ A worker function used for multiprocessing.
+    NOTE: see 'class read_file_to_db' for parameter definitions.
     """
 
-    import mysql.connector import MySQLConnection, Error
     import datetime as dt
-    # make a db connection
-    conn = sqlite3.connect(baseLocation + dbName)
 
     # collect the data 
     t1 = dt.datetime.now()
@@ -151,7 +152,6 @@ def worker(dbName, baseLocation, rad, bmnum, ctr_date,
     rf = read_file_to_db(rad, ctr_date, ftype=ftype, params=params, ffname=ffname)
     print "created an object for " + rad + " for " + str(ctr_date)
     if rf.data is not None:
-
         # move data to db
         rf.move_to_db(conn)
         print ("object has been moved to db")
@@ -160,8 +160,7 @@ def worker(dbName, baseLocation, rad, bmnum, ctr_date,
     print ("creating and moving object to the db took " +\
 	    str((t2-t1).total_seconds() / 60.)) + " mins\n"
 
-    # close db connection
-    conn.close()
+    return
 
 def main():
     """ Call the functions above. Acts as an example code.
@@ -169,59 +168,63 @@ def main():
     The unit process is for reading a day worth of data for a given radar"""
     
     import datetime as dt
-    import sqlite3
     import multiprocessing as mp
+    import sys
+    sys.path.append("../")
+    from mysql_dbutils import db_tools
 
     # input parameters
-
-    # run the code for the following radars in parallel
-    #rad_list = ["bks", "wal", "fhe", "fhw", "cve", "cvw"]
+    sdate = dt.datetime(2015, 1, 1)     # includes sdate
+    edate = dt.datetime(2017, 1, 1)     # does not include edate
     channel = None
     params=['velocity']
     ftype = "fitacf"
     #ftype = "fitex"
     ffname = None
 
-    # make a db connection
-    baseLocation = "../data/sqlite3/"
+    # run the code for the following radars in parallel
+    #rad_list = ["adw", "ade", "hok", "hkw"]
+    rad_list = ["hok"]
 
-            # make a db connection
-            dbName = rad + "_" + ftype + ".sqlite"
-            conn = sqlite3.connect(baseLocation + dbName)
 
-            # close db connection
-            conn.close()
+    # create dbs for radars and save the db connections
+    conn_dict = {} 
+    for rad in rad_list:
+        db_name = rad + "_boxcar_" + ftype 
+        conn_tmp = db_tools.create_db(db_name)
+        conn_tmp.cursor().execute("USE {db}".format(db=db_name))
+        conn_dict[rad] = conn_tmp
 
-    # loop through the dates in all_dates 
+    # create dates, does not include the edate 
+    all_dates = [sdate + dt.timedelta(days=i) for i in range((edate-sdate).days)]
+
+    # loop through the dates
     for ctr_date in all_dates:
 
         # loop through the radars
         for rad in rad_list:
 
-            # Store multiprocesses in a list
-            procs = []
-            for rad in [rad]:
+            worker(conn_dict[rad], rad, ctr_date, ftype, params, ffname)
 
-                # Skip the date if rad has not data for that date
-                if ctr_date not in rads_dict[rad]['date']:
-                    continue
+#            # Store multiprocesses in a list
+#            procs = []
+#
+#            # Creat a processe
+#            p = mp.Process(target=worker, args=(conn_dict[rad], rad, ctr_date,
+#                                                ftype, params, ffname))
+#            procs.append(p)
+#
+#            # Run the process
+#            p.start()
+#
+#        # Make sure the processes terminate
+#        for p in procs:
+#            p.join()
 
 
-                # Creat a processe
-                idx_tmp = rads_dict[rad]['date'].index(ctr_date)
-                bmnum = rads_dict[rad]['beam'][idx_tmp]
-                dbName = rad + "_" + ftype + ".sqlite"
-                p = mp.Process(target=worker,
-                               args=(dbName, baseLocation, rad, bmnum, ctr_date,
-                                     ftype, params, ffname))
-                procs.append(p)
-
-                # Run the process
-                p.start()
-
-            # Make sure the processes terminate
-            for p in procs:
-                p.join()
+    # close db connections
+    for rad in rad_list:
+        conn_dict[rad].close()
 
     return
 
