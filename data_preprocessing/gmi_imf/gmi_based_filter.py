6bb9921eab87ee7d6c80e649ea3d48dee179aef8
MuhammadVT <mamatripik@gmail.com>
2017-08-31 13:45:56 -0400

    finished the code, testing

--- data_preprocessing/gmi_imf/gmi_based_filter.py
+++ data_preprocessing/gmi_imf/gmi_based_filter.py
@@ -1,162 +1,275 @@
-def gmi_based_filter(rad, ftype="fitacf", isKp_based=True, kp_lim=[0,3],
+def gmi_based_filter(rad, output_table, ftype="fitacf", coords="mlt",
+                     isKp_based=True, kp_lim=[0, 2.3],
                      isSymH_based=False, symh_min=-50,
-                     dopsearch_dbName=None, gmi_dbName=None,
-                     baseLocation="./"):
-    """ Selects the quiet time data based on gmag indicies.
+		     config_filename="../mysql_dbconfig_files/config.ini",
+		     section="midlat",
+                     ten_min_median_dbname=None,
+                     gmi_dbname=None, gmi_db_location="../../data/sqlite3/"):
+
+    """ Selects the data based on geomagnetic indicies.
+
+    Parameters
+    ----------
+    rads : str
+        Three-letter name of a radar whose data will be filtered.
+    output_table : str
+        Name of the table where filtered iscat data will be stored.
+    ftype : str
+        SuperDARN LOS data file type
+    coords : str
+        Coordinate systems. valid inputs are "geo" or "mlt".
+    isKp_based : bool
+        If set to True, data will be filtered based on Kp.
+    kp_lim : list
+        The range of Kp. The range bondaries are inclusive.
+    isSymH_based : bool
+        If set to True, data will be filtered based on SymH.
+    symh_min : float
+        The lower limit for SYMH.
+    config_filename: str
+        name and path of the configuration file
+    section: str, default to "midlat"
+        section of database configuration
+    ten_min_median_dbname : str
+        Name of the db where ten min median iscat data is stored.
+        Default to None.
+    gmi_dbname : str
+        Name of the sqlite3 db where geomagnetic indices are stored.
+        Default to None.
+    gmi_db_location : str
+        The path to gmi_dbname sqlite db.
+        Default to None.
+
+    Returns
+    -------
+    Nothing
 
     Note
     ----
-        SymH base filter has not been implemented yet
+    SymH base filter has not been implemented yet
     
     """
 
-    import sys
-    sys.path.append("../../")
-    from dbtools.db.connection.Connector import Connectors, Executor
     import datetime as dt
     import sqlite3
     import os
+    import sys
+    sys.path.append("../../")
+    from mysql.connector import MySQLConnection
+    from mysql_dbutils.db_config import db_config
+    import logging
 
-    # make db connection for dopsearch
-    if dopsearch_dbName is None:
-        dopsearch_dbName = "dopsearch_" + rad + "_" + ftype + ".sqlite"
-    conn_dop = Connectors(baseLocation = baseLocation, dbName = dopsearch_dbName,
-                      isInMem = False, isAutoSave = False)
-    executor_dop = Executor(conn = conn_dop)
+    # construct db names and table names
+    if ten_min_median_dbname is None:
+        ten_min_median_dbname = "ten_min_median_" + coords + "_" + ftype
+    if gmi_dbname is None:
+        gmi_dbname = "gmi_imf.sqlite"
+    input_table = rad + "_" + ftype
 
-    # make db connection for gmi
-    if gmi_dbName is None:
-        gmi_dbName = "gmi_imf.sqlite"
-    conn_gmi = Connectors(baseLocation = "./gmi_imf/", dbName = gmi_dbName,
-                      isInMem = False, isAutoSave = False)
-    executor_gmi = Executor(conn = conn_gmi)
+    # make db connection to geomag indicies data
+    conn_gmi = sqlite3.connect(database =gmi_db_location + gmi_dbname,
+                               detect_types=sqlite3.PARSE_DECLTYPES)
+    cur_gmi = conn_gmi.cursor()
 
-#    if isSymH_based:
-#        tbl_sym = "symh"
-#        executor_gmi._set_command("SELECT datetime FROM {tb} WHERE symh >= {sym_min}"\
-#                                   .format(tb=tbl_sym, symh_min=symh_min))
-#        executor_gmi._execute_command()
-#        dtms = executor_gmi.conn.cursor.fetchall()
+    # make db connection for ten-min median filtered iscat data
+    # read the db config info
+    config =  db_config(config_filename=config_filename, section=section)
+    config_info = config.read_db_config()
 
+    # make a connection
+    try:
+        conn_iscat = MySQLConnection(database=ten_min_median_dbname, **config_info)
+        cur_iscat = conn_iscat.cursor(buffered=True)
+    except Exception, e:
+        logging.error(e, exc_info=True)
 
-    # get all the tables names in dopsearch db
-    executor_dop._set_command("SELECT name FROM sqlite_master WHERE type = 'table'")
-    executor_dop._execute_command()
-    tbl_names = executor_dop.conn.cursor.fetchall()
-    tbl_names = [x[0] for x in tbl_names]
+    # select the time intervals base on SymH
+    if isSymH_based:
+        pass
 
-    # select the time intervals base on kp_lim
+    # select the time intervals based on Kp 
     if isKp_based:
         tbl_nm = "kp"
-        command = "SELECT datetime FROM {tb} WHERE kp < {kp_min} OR\
-                  kp >= {kp_max}".format(tb=tbl_nm, kp_min=kp_lim[0],kp_max=kp_lim[1])
-
-        executor_gmi._set_command(command)
-        executor_gmi._execute_command()
-        dtms = executor_gmi.conn.cursor.fetchall()
+        command = "SELECT datetime FROM {tb} WHERE kp BETWEEN {kp_min} AND {kp_max}"
+        command = command.format(tb=tbl_nm, kp_min=kp_lim[0],kp_max=kp_lim[1])
+        cur_gmi.execute(command)
+        dtms = cur_gmi.fetchall()
         dtms = [x[0] for x in dtms]
 
     # close conn_gmi opened for gmi db
-    conn_gmi.connection.close()
+    conn_gmi.close()
 
-    # store rowids_dop in a tmp_db
-    tmp_db = baseLocation + "tmp_db_" + rad
-    tmp_con = sqlite3.connect(tmp_db+".sqlite")
-    # attache the tmp_db database into dopsearch db
-    commd = "ATTACH DATABASE '{tmp_db}' AS 'tmpdb'".format(tmp_db=tmp_db+".sqlite")
-    conn_dop.cursor.execute(commd)
+    # create the output table where the filtered iscat data will be stored
+    if coords == "mlt":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel float(9,2)," +\
+                  " mag_glatc float(7,2)," +\
+                  " mag_gltc float(8,2)," +\
+                  " mag_gazmc TINYINT(5)," +\
+                  " datetime DATETIME, " +\
+                  " rad VARCHAR(3), " +\
+                  " CONSTRAINT filtered PRIMARY KEY (" +\
+                  "mag_glatc, mag_gltc, mag_gazmc, datetime, rad))"
+        command_insert = "INSERT IGNORE INTO {tb2} (vel, mag_glatc, mag_gltc, " +\
+                  "mag_gazmc, datetime, rad) VALUES (%s, %s, %s, %s, %s, %s)"
+    elif coords == "geo":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel float(9,2)," +\
+                  " geo_glatc float(7,2)," +\
+                  " geo_gltc float(8,2)," +\
+                  " geo_gazmc TINYINT(5)," +\
+                  " datetime DATETIME, " +\
+                  " rad VARCHAR(3), " +\
+                  " CONSTRAINT filtered PRIMARY KEY (" +\
+                  "geo_glatc, geo_gltc, geo_gazmc, datetime, rad))"
+        command_insert = "INSERT IGNORE INTO {tb2} (vel, geo_glatc, geo_gltc, " +\
+                  "geo_gazmc, datetime, rad) VALUES (%s, %s, %s, %s, %s, %s)"
 
-    # filter dopsearch db based on kp
-    # do it one table at a time
-    for tbl in tbl_names:
+    command = command.format(tb=output_table)
+    command_insert = command_insert.format(tb2=output_table)
+    try:
+        cur_iscat.execute(command)
+    except Exception, e:
+        logging.error(e, exc_info=True)
 
-        tmp_cur = tmp_con.cursor()    
-        # drop of the tmp table if it exists
-        tmp_cur.execute("DROP TABLE IF EXISTS tmp")
+    # filter iscat db based on kp
+    # loop through every 3 hour interval of kp
+    for dtm in dtms:
+        sdtm = dtm
+        edtm = sdtm + dt.timedelta(hours=3)
+        command = "SELECT * FROM {tb} WHERE datetime BETWEEN '{sdtm}' " +\
+                  "AND '{edtm}'"
+        command = command.format(tb=input_table, sdtm=sdtm, edtm=edtm)
 
-        # create a tmp table inside the tmp db
-        tmp_cur.execute("CREATE TABLE tmp (rid INTEGER PRIMARY KEY)")
+        # check the db connection before fetching 
+        if not conn_iscat.is_connected():
+            conn_iscat.reconnect()
 
-        # loop through every 3 hour interval of kp
-        for dtm in dtms:
-            sdtm = dtm
-            edtm = sdtm + dt.timedelta(hours=3)
-
-            # select row ids of needed records
-            #command = "SELECT rowid FROM {tb} WHERE DATETIME(datetime) >= ?\
-            #          AND DATETIME(datetime) < ?".format(tb=tbl)
-            command = "SELECT rowid FROM {tb} WHERE datetime >= ?\
-                      AND datetime < ?".format(tb=tbl)
-            conn_dop.cursor.execute(command, (sdtm, edtm))
-            rowids_tmp = executor_dop.conn.cursor.fetchall()
-            
-            if len(rowids_tmp) > 0:
-                rowids_tmp = [x[0] for x in rowids_tmp]
-                for rwid in rowids_tmp:
-
-                    # populate the tmp table in the memory db
-                    tmp_cur.execute("INSERT OR IGNORE INTO tmp (rid) VALUES (?)", (rwid,))
+        # fetch the data
+        try:
+	    cur_iscat.execute(command)
+	    rows = cur_iscat.fetchall()
+        except Exception, e:
+            logging.error(e, exc_info=True)
         
-        # commit the change
-        tmp_con.commit()
+        # insert the data into a table
+        if rows:
+            for rw in rows:
+                vel, lat, lt, azm, dtm = rw
 
-        # delete unwanted records from dopsearch db
-        command = "DELETE FROM {tb} WHERE rowid IN (SELECT rid FROM tmpdb.tmp)"\
-                  .format(tb=tbl)
-        conn_dop.cursor.execute(command)
+                # check the db connection before inserting
+                if not conn_iscat.is_connected():
+                    conn_iscat.reconnect()
+                # insert the data
+                try:
+                    cur_iscat.execute(command_insert, (vel, lat, lt, azm, dtm, rad))
+                except Exception, e:
+                    logging.error(e, exc_info=True)
 
-        # commit the change
-        conn_dop.connection.commit()
-        print "finished filtering " + tbl + " of " + rad
-        
-    # detach the tmpdb from dopsearch db
-    conn_dop.cursor.execute("DETACH DATABASE 'tmpdb'")
-            
+    # check the db connection before committing
+    if not conn_iscat.is_connected():
+        conn_iscat.reconnect()
+    # commit the change
+    try:
+        conn_iscat.commit()
+    except Exception, e:
+        logging.error(e, exc_info=True)
+
     # close the connections
-    tmp_con.close()
-    conn_dop.connection.close()
-
-    # remove the tmp_db
-    os.system("rm " + tmp_db + ".sqlite")
-    print "removed " + tmp_db + ".sqlite"
-
-
-def worker(rads, baseLocation, kp_lim):
-
-    # input parameters
-    ftype = "fitacf"
-    dopsearch_dbName= None
-    gmi_dbName = None
-
-    for rad in rads:
-        gmi_based_filter(rad, ftype=ftype, isKp_based=True, kp_lim=kp_lim,
-                         dopsearch_dbName=dopsearch_dbName, gmi_dbName=gmi_dbName,
-                         baseLocation=baseLocation)
-        print "finish filtering " + baseLocation + "dopsearch_" + rad + "_" + ftype + ".sqlite"
+    conn_iscat.close()
 
     return
 
-if __name__ == "__main__":
+def worker(rad, output_table, ftype="fitacf", coords="mlt",
+           isKp_based=True, kp_lim=[0, 2.3],
+           isSymH_based=False, symh_min=-50,
+	   config_filename="../../mysql_dbconfig_files/config.ini",
+	   section="midlat",
+           ten_min_median_dbname=None,
+           gmi_dbname=None, gmi_db_location="../../data/sqlite3/"):
+    """ A worker function for parallel computing"""
+
+    # filter the data
+    print("Start filtering data from " + rad)
+    t1 = dt.datetime.now()
+    gmi_based_filter(rad, output_table, ftype=ftype, coords=coords,
+                     isKp_based=isKp_based, kp_lim=kp_lim,
+                     isSymH_based=isSymH_based, symh_min=symh_min,
+		     config_filename="../../mysql_dbconfig_files/config.ini",
+		     section="midlat",
+                     ten_min_median_dbname=ten_min_median_dbname,
+                     gmi_dbname=gmi_dbname,
+                     gmi_db_location=gmi_db_location)
+    t2 = dt.datetime.now()
+    print("finish filtering data from " + rad +\
+          ", which took " + str((t2-t1).total_seconds() / 60.) + " mins\n")
+
+    return
+
+def main(run_in_parallel=False):
+    """ Executes the above functions in parallel.
+    Unit process runs for a single radar"""
+
     import multiprocessing
 
-    seasons = ["winter", "summer", "equinox"]
-    #seasons = ["winter"]
-    rads_list = [["bks", "wal", "fhe"], ["fhw", "cve", "cvw"]]
-    #seasons = ["winter"]
-    #rads_list = [["bks"]]
-    #kp_lim = [0, 3]
-    kp_lim = [0, 1]
-
+    # initialize parameters
+    rad_list = ["hok", "hkw"]
+    kp_lim = [0, 2.3]    # the range boundaries are inclusive
+    coords = "mlt"
+    ftype="fitacf"
+    output_table = "_".join(rad_list) + "_kp_" +\
+                   "_to_".join([str(x) for x in kp_lim]) + "_" + ftype
+    isKp_based=True
+    isSymH_based=False
+    symh_min=-50
+    ten_min_median_dbname=None
+    gmi_dbname=None
+    gmi_db_location="../../data/sqlite3/"
     
-    jobs = []
-    for season in seasons:
-        #baseLocation="./" + season + "/kp_l_3/" + "data_in_geo" + "/"
-        #baseLocation="./" + season + "/kp_l_3/" + "data_in_mlt" + "/"
-        #baseLocation="./" + season + "/kp_l_2/" + "data_in_geo" + "/"
-        #baseLocation="./" + season + "/kp_l_2/" + "data_in_mlt" + "/"
-        #baseLocation="./" + season + "/kp_l_1/" + "data_in_geo" + "/"
-        baseLocation="./" + season + "/kp_l_1/" + "data_in_mlt" + "/"
-        for i in range(len(rads_list)):
-            p = multiprocessing.Process(target=worker, args=(rads_list[i], baseLocation, kp_lim))
-            jobs.append(p)
+    # run in parallel
+    # store the multiprocess
+    procs = []
+    # loop through radars
+    for rad in rad_list: 
+        if run_in_parallel:
+            # cteate a process
+            worker_kwargs = {"ftype":ftype, "coords":coords,
+			     "isKp_based": isKp_based, "kp_lim": kp_lim,
+			     "isSymH_based": isSymH_based, "symh_min": symh_min,
+                             "config_filename":"../../mysql_dbconfig_files/config.ini",
+                             "section":"midlat",
+			     "ten_min_median_dbname": ten_min_median_dbname,
+			     "gmi_dbname": gmi_dbname,
+			     "gmi_db_location": gmi_db_location}
+            p = mp.Process(target=worker, args=(rad, output_table),
+                           kwargs=worker_kwargs)
+            procs.append(p)
+            
+            # run the process
             p.start()
+            
+        else:
+            # run in serial
+	    worker(rad, output_table, ftype=ftype, coords=coords,
+			 isKp_based=isKp_based, kp_lim=kp_lim,
+			 isSymH_based=isSymH_based, symh_min=symh_min,
+			 config_filename="../../mysql_dbconfig_files/config.ini",
+			 section="midlat", 
+			 ten_min_median_dbname=ten_min_median_dbname,
+			 gmi_dbname=gmi_dbname,
+			 gmi_db_location=gmi_db_location)
+            
+    if run_in_parallel:
+        # make sure the processes terminate
+        for p in procs:
+            p.join()
+
+if __name__ == "__main__":
+    import datetime as dt
+    t1 = dt.datetime.now()
+    main(run_in_parallel=False)
+    t2 = dt.datetime.now()
+    print("Finishing gmi_based filtering took " +\
+    str((t2-t1).total_seconds() / 60.) + " mins\n")
+
+
