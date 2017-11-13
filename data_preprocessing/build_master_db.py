c96df79055e07190c8151c0e7016b37d17e014ed
MuhammadVT <mamatripik@gmail.com>
2017-11-13 08:58:35 -0500

    create database for subgrouping the data

--- data_preprocessing/build_master_db.py
+++ data_preprocessing/build_master_db.py
@@ -157,9 +157,9 @@ def build_master_table(input_table, output_table, ftype="fitacf", coords="mlt",
 
     return
 
-def master_summary(input_table, output_table, coords="mlt", db_name=None,
-                   config_filename="../mysql_dbconfig_files/config.ini",
-                   section="midlat"):
+def master_summary_by_season(input_table, output_table, coords="mlt", db_name=None,
+                             config_filename="../mysql_dbconfig_files/config.ini",
+                             section="midlat"):
     
     """ stores the summay statistics of the data in master table into 
     a different table in the same database.
@@ -318,13 +318,353 @@ def master_summary(input_table, output_table, coords="mlt", db_name=None,
 
     return
 
+def master_summary_by_radar_season(input_table, output_table, coords="mlt", db_name=None,
+                                   radar_pair=["cvw", "cve"],
+                                   config_filename="../mysql_dbconfig_files/config.ini",
+                                   section="midlat"):
+    
+    """ stores the summay statistics of the data in master table into 
+    a different table in the same database.
+    Time and rad informatin are all lost at this point.
+    NOTE: this function is only used selecet for pairs of radars from the
+    six U.S. radars. 
+
+    Parameters
+    ----------
+    input_table : str
+        Name of a master table in master db
+    output_table : str
+        Name of a master_summary table in master db
+    coords : str
+        Coordinates in which the binning process took place.
+        Default to "mlt, can be "geo" as well. 
+    radar_pair = list
+        a pair of three-character radar names
+    config_filename: str
+        name and path of the configuration file
+    section: str, default to "midlat"
+        section of database configuration
+    db_name : str, default to None
+        Name of the master db
+
+    Returns
+    -------
+    Nothing
+
+    """
+    import datetime as dt
+    import numpy as np 
+    from mysql.connector import MySQLConnection
+    import sys
+    sys.path.append("../")
+    from mysql_dbutils.db_config import db_config
+    import logging
+
+    # construct a db name
+    if db_name is None:
+        db_name = "master_" + coords + "_" +ftype
+
+    # read db config info
+    config =  db_config(config_filename=config_filename, section=section)
+    config_info = config.read_db_config()
+
+    # make a connection to master db
+    try:
+        conn = MySQLConnection(database=db_name, **config_info)
+        cur = conn.cursor(buffered=True)
+    except Exception, e:
+        logging.error(e, exc_info=True)
+
+    # create a table
+    if coords == "mlt":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel_mean float(9,2)," +\
+                  " vel_median float(9,2)," +\
+                  " vel_std float(9,2)," +\
+                  " vel_count INT," +\
+                  " mag_glatc float(7,2)," +\
+                  " mag_gltc float(8,2)," +\
+                  " mag_gazmc SMALLINT," +\
+                  " season VARCHAR(8), " +\
+                  " CONSTRAINT grid_season PRIMARY KEY (" +\
+                  "mag_glatc, mag_gltc, mag_gazmc, season))"
+    elif coords == "geo":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel_mean float(9,2)," +\
+                  " vel_median float(9,2)," +\
+                  " vel_std float(9,2)," +\
+                  " vel_count INT," +\
+                  " geo_glatc float(7,2)," +\
+                  " geo_gltc float(8,2)," +\
+                  " geo_gazmc SMALLINT," +\
+                  " season VARCHAR(8), " +\
+                  " CONSTRAINT grid_season PRIMARY KEY (" +\
+                  "geo_glatc, geo_gltc, geo_gazmc, season))"
+
+    command = command.format(tb=output_table)
+    try:
+        cur.execute(command)
+    except Exception, e:
+        logging.error(e, exc_info=True)
+
+    if coords == "mlt":
+	command = "SELECT AVG(vel), STD(vel), COUNT(vel), " +\
+                  "mag_glatc, mag_gltc, mag_gazmc, season " +\
+		  "FROM {tb1} "+\
+                  "WHERE rad='{rad1}' OR rad='{rad2}' "+\
+                  "GROUP BY mag_glatc, mag_gltc, mag_gazmc, season"
+    elif coords == "geo":
+	command = "SELECT AVG(vel), STD(vel), COUNT(vel), " +\
+                  "geo_glatc, geo_gltc, geo_gazmc, season " +\
+		  "FROM {tb1} "+\
+                  "WHERE rad='{rad1}' OR rad='{rad2}' "+\
+                  "GROUP BY geo_glatc, geo_gltc, geo_gazmc, season"
+    command = command.format(tb1=input_table, rad1=radar_pair[0],
+                             rad2=radar_pair[1])
+
+    # check the db connection before fetching 
+    if not conn.is_connected():
+	conn.reconnect()
+    # fetch the data
+    try:
+	cur.execute(command)
+    except Exception, e:
+	logging.error(e, exc_info=True)
+    rows = cur.fetchall()
+
+    # insert the data into a table
+    if rows:
+	if coords == "mlt":
+	    command = "INSERT IGNORE INTO {tb2} (vel_mean, vel_median, vel_std, vel_count, " +\
+                      "mag_glatc, mag_gltc, mag_gazmc, season) " +\
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	elif coords == "geo":
+	    command = "INSERT IGNORE INTO {tb2} (vel_mean, vel_median, vel_std, vel_count, " +\
+                      "geo_glatc, geo_gltc, geo_gazmc, season) " +\
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	command = command.format(tb2=output_table)
+	for rw in rows:
+            vel_mean, vel_std, vel_count, lat, lt, azm, season =rw
+
+            # find median
+            if coords == "mlt":
+                command_tmp = "SELECT vel FROM {tb1} " +\
+                              "WHERE mag_glatc={lat} and mag_gltc={lt} and "+\
+                              "mag_gazmc={azm} and season='{season}'"
+            elif coords == "geo":
+                command_tmp = "SELECT vel FROM {tb1} " +\
+                              "WHERE geo_glatc={lat} and geo_gltc={lt} and "+\
+                              "geo_gazmc={azm} and season='{season}'"
+            command_tmp = command_tmp.format(tb1=input_table, lat=lat, lt=lt,
+                                             azm=azm, season=season)
+            try:
+                cur.execute(command_tmp)
+            except Exception, e:
+                logging.error(e, exc_info=True)
+            vels_tmp = cur.fetchall()
+            vels_tmp = [x[0] for x in vels_tmp]
+            vel_median = np.median(vels_tmp)
+
+	    # check the db connection before inserting
+	    if not conn.is_connected():
+		conn.reconnect()
+	    # insert the data
+	    try:
+		cur.execute(command,
+                            (round(vel_mean,2), round(vel_median,2), round(vel_std,2),
+                             vel_count, lat, lt, azm, season))
+	    except Exception, e:
+		logging.error(e, exc_info=True)
+
+    # check the db connection before committing
+    if not conn.is_connected():
+	conn.reconnect()
+    # commit the change
+    try:
+	conn.commit()
+    except Exception, e:
+	logging.error(e, exc_info=True)
+
+    # close db connections
+    conn.close()
+
+    return
+
+def master_summary_by_year_season(input_table, output_table, coords="mlt", db_name=None,
+                                  selected_years=[2011, 2012],
+                                  config_filename="../mysql_dbconfig_files/config.ini",
+                                  section="midlat"):
+    
+    """ stores the summay statistics of the data in master table into 
+    a different table in the same database.
+    Time and rad informatin are all lost at this point.
+
+    Parameters
+    ----------
+    input_table : str
+        Name of a master table in master db
+    output_table : str
+        Name of a master_summary table in master db
+    coords : str
+        Coordinates in which the binning process took place.
+        Default to "mlt, can be "geo" as well. 
+    config_filename: str
+        name and path of the configuration file
+    section: str, default to "midlat"
+        section of database configuration
+    db_name : str, default to None
+        Name of the master db
+
+    Returns
+    -------
+    Nothing
+
+    """
+    import datetime as dt
+    import numpy as np 
+    from mysql.connector import MySQLConnection
+    import sys
+    sys.path.append("../")
+    from mysql_dbutils.db_config import db_config
+    import logging
+
+    # construct a db name
+    if db_name is None:
+        db_name = "master_" + coords + "_" +ftype
+
+    # read db config info
+    config =  db_config(config_filename=config_filename, section=section)
+    config_info = config.read_db_config()
+
+    # make a connection to master db
+    try:
+        conn = MySQLConnection(database=db_name, **config_info)
+        cur = conn.cursor(buffered=True)
+    except Exception, e:
+        logging.error(e, exc_info=True)
+
+    # create a table
+    if coords == "mlt":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel_mean float(9,2)," +\
+                  " vel_median float(9,2)," +\
+                  " vel_std float(9,2)," +\
+                  " vel_count INT," +\
+                  " mag_glatc float(7,2)," +\
+                  " mag_gltc float(8,2)," +\
+                  " mag_gazmc SMALLINT," +\
+                  " season VARCHAR(8), " +\
+                  " CONSTRAINT grid_season PRIMARY KEY (" +\
+                  "mag_glatc, mag_gltc, mag_gazmc, season))"
+    elif coords == "geo":
+        command = "CREATE TABLE IF NOT EXISTS {tb}" +\
+                  "(vel_mean float(9,2)," +\
+                  " vel_median float(9,2)," +\
+                  " vel_std float(9,2)," +\
+                  " vel_count INT," +\
+                  " geo_glatc float(7,2)," +\
+                  " geo_gltc float(8,2)," +\
+                  " geo_gazmc SMALLINT," +\
+                  " season VARCHAR(8), " +\
+                  " CONSTRAINT grid_season PRIMARY KEY (" +\
+                  "geo_glatc, geo_gltc, geo_gazmc, season))"
+    
+    command = command.format(tb=output_table)
+    try:
+        cur.execute(command)
+    except Exception, e:
+        logging.error(e, exc_info=True)
+
+    if coords == "mlt":
+	command = "SELECT AVG(vel), STD(vel), COUNT(vel), " +\
+                  "mag_glatc, mag_gltc, mag_gazmc, season " +\
+		  "FROM {tb1} "+\
+                  "WHERE YEAR(datetime) IN {years} "+\
+                  "GROUP BY mag_glatc, mag_gltc, mag_gazmc, season"
+    elif coords == "geo":
+	command = "SELECT AVG(vel), STD(vel), COUNT(vel), " +\
+                  "geo_glatc, geo_gltc, geo_gazmc, season " +\
+		  "FROM {tb1} "+\
+                  "WHERE YEAR(datetime) IN {years}"+\
+                  "GROUP BY geo_glatc, geo_gltc, geo_gazmc, season"
+    command = command.format(tb1=input_table, years=tuple(selected_years))
+    print command
+
+    # check the db connection before fetching 
+    if not conn.is_connected():
+	conn.reconnect()
+    # fetch the data
+    try:
+	cur.execute(command)
+    except Exception, e:
+	logging.error(e, exc_info=True)
+    rows = cur.fetchall()
+
+    # insert the data into a table
+    if rows:
+	if coords == "mlt":
+	    command = "INSERT IGNORE INTO {tb2} (vel_mean, vel_median, vel_std, vel_count, " +\
+                      "mag_glatc, mag_gltc, mag_gazmc, season) " +\
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	elif coords == "geo":
+	    command = "INSERT IGNORE INTO {tb2} (vel_mean, vel_median, vel_std, vel_count, " +\
+                      "geo_glatc, geo_gltc, geo_gazmc, season) " +\
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	command = command.format(tb2=output_table)
+	for rw in rows:
+            vel_mean, vel_std, vel_count, lat, lt, azm, season =rw
+
+            # find median
+            if coords == "mlt":
+                command_tmp = "SELECT vel FROM {tb1} " +\
+                              "WHERE mag_glatc={lat} and mag_gltc={lt} and "+\
+                              "mag_gazmc={azm} and season='{season}'"
+            elif coords == "geo":
+                command_tmp = "SELECT vel FROM {tb1} " +\
+                              "WHERE geo_glatc={lat} and geo_gltc={lt} and "+\
+                              "geo_gazmc={azm} and season='{season}'"
+            command_tmp = command_tmp.format(tb1=input_table, lat=lat, lt=lt,
+                                             azm=azm, season=season)
+            try:
+                cur.execute(command_tmp)
+            except Exception, e:
+                logging.error(e, exc_info=True)
+            vels_tmp = cur.fetchall()
+            vels_tmp = [x[0] for x in vels_tmp]
+            vel_median = np.median(vels_tmp)
+
+	    # check the db connection before inserting
+	    if not conn.is_connected():
+		conn.reconnect()
+	    # insert the data
+	    try:
+		cur.execute(command,
+                            (round(vel_mean,2), round(vel_median,2), round(vel_std,2),
+                             vel_count, lat, lt, azm, season))
+	    except Exception, e:
+		logging.error(e, exc_info=True)
+
+    # check the db connection before committing
+    if not conn.is_connected():
+	conn.reconnect()
+    # commit the change
+    try:
+	conn.commit()
+    except Exception, e:
+	logging.error(e, exc_info=True)
+
+    # close db connections
+    conn.close()
+
+    return
+
 def main(master_table=True, master_summary_table=True):
     import datetime as dt
     import logging
 
     # create a log file to which any error occured between client and
     # MySQL server communication will be written.
-    logging.basicConfig(filename="./log_files/master_table_kp_00_to_23_ade_adw.log",
+    logging.basicConfig(filename="./log_files/master_table_kp_00_to_23_six_rads_2015_2016.log",
                         level=logging.INFO)
 
     # input parameters
@@ -333,10 +673,25 @@ def main(master_table=True, master_summary_table=True):
     #input_table_2 = "master_hok_hkw_kp_00_to_23"
     #output_table_2 = "master_summary_hok_hkw_kp_00_to_23"
 
-    input_table_1 = "ade_adw_kp_00_to_23_fitacf"
-    output_table_1 = "master_ade_adw_kp_00_to_23"
-    input_table_2 = "master_ade_adw_kp_00_to_23"
-    output_table_2 = "master_summary_ade_adw_kp_00_to_23"
+    #input_table_1 = "ade_adw_kp_00_to_23_fitacf"
+    #output_table_1 = "master_ade_adw_kp_00_to_23"
+    #input_table_2 = "master_ade_adw_kp_00_to_23"
+    #output_table_2 = "master_summary_ade_adw_kp_00_to_23"
+
+
+
+    selected_years=[2015, 2016]
+    #grouped_month=[[1,2], [3,4], [5,6], [7,8], [9,10], [11, 12]]
+
+
+    #rads_txt = "bks_wal"
+    rads_txt = "six_rads"
+    years_txt = "_years_" + "_".join([str(x) for x in selected_years])
+    input_table_1 = rads_txt + "_kp_00_to_23_fitacf"
+    output_table_1 = "master_" + rads_txt + "_kp_00_to_23"
+    input_table_2 = "master_" + rads_txt + "_kp_00_to_23"
+    output_table_2 = "master_summary_" + rads_txt + "_kp_00_to_23" + years_txt
+    #output_table_2 = "master_summary_cve_cvw_kp_00_to_23" + years_txt
 
     ftype = "fitacf"
     coords = "mlt"
@@ -357,14 +712,26 @@ def main(master_table=True, master_summary_table=True):
     if master_summary_table:
         # build a summary table
         print "building a master_summary table"
-        master_summary(input_table_2, output_table_2, coords=coords,
-                       db_name=output_dbname,
-                       config_filename="../mysql_dbconfig_files/config.ini",
-                       section="midlat")
+#        master_summary_by_season(input_table_2, output_table_2, coords=coords,
+#                               db_name=output_dbname,
+#                               config_filename="../mysql_dbconfig_files/config.ini",
+#                               section="midlat")
+#        master_summary_by_radar_season(input_table_2, output_table_2, coords=coords,
+#                                       db_name=output_dbname,
+#                                       radar_pair=["cve", "cvw"],
+#                                       config_filename="../mysql_dbconfig_files/config.ini",
+#                                       section="midlat")
+
+        master_summary_by_year_season(input_table_2, output_table_2, coords=coords,
+                                       db_name=output_dbname,
+                                       selected_years=selected_years,
+                                       config_filename="../mysql_dbconfig_files/config.ini",
+                                       section="midlat")
+
         print "A master_summary has been build"
 
     return
 
 if __name__ == "__main__":
-    main(master_table=True, master_summary_table=True)
+    main(master_table=False, master_summary_table=True)
 
