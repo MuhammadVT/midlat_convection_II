5e944ba17ff61000194cc9b7f40e070afe398d3b
MuhammadVT <mamatripik@gmail.com>
2018-07-24 08:08:22 -0400

    add run_in_parallel arg and fix path issue

--- data_preprocessing/boxcarfilter.py
+++ data_preprocessing/boxcarfilter.py
@@ -1,9 +1,7 @@
 # load modules
 import sys
 sys.path.append("../classification_of_HF_radar_backscatter")
-
-sys.path.append("/home/muhammad/Documents/Important/sd-data_update_indices")
-from  iscat_identifier import prepare_file
+from iscat_identifier import prepare_file
 import datetime as dt
 from davitpy.pydarn.sdio.fetchUtils import fetch_local_files
 import os
@@ -11,7 +9,8 @@ import multiprocessing as mp
 import logging
 #davitpy.rcParams['verbosity'] = "debug"
 
-def do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel, n_jobs=None):
+def do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel,
+                       n_jobs=None, run_in_parallel=True):
 
     """ fetches, concatenates and does boxcar median filtering for data
     in the time iterval between sctr_day, ectr_day for a given radar.
@@ -39,7 +38,6 @@ def do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel, n_jobs=None
     Nothing
         
     """
-
     
     # calculate number of days 
     sctr_day = dt.datetime(sctr_day.year, sctr_day.month, sctr_day.day)
@@ -60,50 +58,61 @@ def do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel, n_jobs=None
     for rad in rad_list:
 
         # create tmpdir to store the data
-        rad_tmp_dir = rad + "_tmp"
-        os.system("mkdir -p " + rad_tmp_dir)
-        rad_tmp_dir = os.getcwd() + "/data/"+ rad_tmp_dir + "/"
+        rad_tmp_dir = "../data/" + rad + "_tmp/"
+        if not os.path.exists(rad_tmp_dir):
+            os.system("mkdir -p " + rad_tmp_dir)
+        #rad_tmp_dir = os.getcwd() + "/data/"+ rad_tmp_dir + "/"
         print rad_tmp_dir + " is created"
 
 
         # cteate tmpdirs, one for each n_jobs
         tmp_dirs = []
-        for j in range(n_jobs):
-
+        if run_in_parallel:
+            n_dirs = n_jobs
+        else:
+            n_dirs = 1 
+        for j in range(n_dirs):
             # create tmpdirs to store the data, one tmpdir for each process
-            tmp_dir = rad + "_tmp" + "_" + str(j)
+            tmp_dir = "../data/" + rad + "_tmp" + "_" + str(j) +"/"
             os.system("mkdir -p " + tmp_dir)
-            tmp_dir = os.getcwd() + "/data/"+ tmp_dir + "/"
+            #tmp_dir = os.getcwd() + "/data/"+ tmp_dir + "/"
             print tmp_dir + " is created"
             tmp_dirs.append(tmp_dir)
         
         # iter through the days, one day at a time
         i = 0
         while 1:
-
-            # run n_jobs in parallel
-            dts_tmp = dt_range[i*n_jobs:(i+1)*n_jobs]
-            i = i + 1
-            if len(dts_tmp) == 0:
-                break
+            if run_in_parallel:
+                # run n_jobs in parallel
+                dts_tmp = dt_range[i*n_jobs:(i+1)*n_jobs]
+                i = i + 1
+                if len(dts_tmp) == 0:
+                    break
+                else:
+                    procs = []
+                    # send jobs in parallel
+                    for j, ctr_dtm in enumerate(dts_tmp):
+                       p = mp.Process(target=worker, args=(rad, ctr_dtm, ftype, channel, tmp_dirs[j]))
+                       procs.append(p)
+                       p.start()
             else:
-                procs = []
-                # send jobs in parallel
-                for j, ctr_dtm in enumerate(dts_tmp):
-                   p = mp.Process(target=worker, args=(rad, ctr_dtm, ftype, channel, tmp_dirs[j]))
-                   procs.append(p)
-                   p.start()
+                ctr_dtm = dt_range[i]
+                i = i + 1
+                worker(rad, ctr_dtm, ftype, channel, tmp_dirs[0])
+                if i >= len(dt_range):
+                    break
 
+            if run_in_parallel:
                 # exit the completed processes
                 for p in procs:
                     p.join()
 
         # move processed data from tmpdirs of all processes to a single tmpdir
-        for j in range(n_jobs):
+        for j in range(n_dirs):
             os.system("mv " + tmp_dirs[j] + "* " + rad_tmp_dir)
 
         # remove tmpdirs
-        os.system("rm -rf " + rad + "_tmp_*")
+        os.system("rm -rf " + "../data/" + rad + "_tmp_*")
         print "tmpdirs have been deleted"
 
     return
@@ -151,37 +160,40 @@ def worker(rad, ctr_dtm, ftype, channel, tmp_dir):
 
     return
 
-def main():
-    """Execute the codes above"""
+def main(run_in_parallel=True):
+    """Executes the codes above"""
 
     # input parameters
     # create center datetimes 
     """ NOTE: Set the channel value correctly. """
-
-    yrs = [2015, 2016]
-    #yrs = [2016]
+    
+    n_jobs = 20
+    #yrs = [2015, 2016]
+    yrs = [2017]
     for yr in yrs:
         ftype = "fitacf"
         #ftype = "fitex"
         sctr_day = dt.datetime(yr,1,1)
-        ectr_day = dt.datetime(yr+1,1,1)
+        ectr_day = dt.datetime(yr+1,7,1)
 
-        #rad_list = ["wal", "fhe", "fhw", "cve", "cvw"]
+        rad_list = ["bks", "wal", "fhe", "fhw", "cve", "cvw"]
         #rad_list = ["bks",  "wal", "fhe"]
         #rad_list = ["fhw", "cve", "cvw"]
         #rad_list = ["bks"]
-        rad_list = ["tig", "unw"]
+        #rad_list = ["tig", "unw"]
         #rad_list = ["bpk", "hok", "hkw"]
         #rad_list = ["hok", "hkw"]
         #rad_list = ["ade", "adw"]
         channel = None
         #channel = '.'
 
-        do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel, n_jobs=20)
+        do_boxcarfiltering(sctr_day, ectr_day, rad_list, ftype, channel,
+                           n_jobs=n_jobs, run_in_parallel=run_in_parallel)
 
     return
 
 if __name__ == "__main__":
-    main()
+    main(run_in_parallel=False)
+    #main(run_in_parallel=True)
 
 
