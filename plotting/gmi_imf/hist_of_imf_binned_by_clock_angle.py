0fb04312cb6745fbdaa1dbefbae167582038b52e
MuhammadVT <mamatripik@gmail.com>
2018-07-27 17:26:15 -0400

    plot hist of 3 by 3 IMF clock angle bins

---
+++ plotting/gmi_imf/hist_of_imf_binned_by_clock_angle.py
@@ -0,0 +1,181 @@
+import matplotlib
+matplotlib.use('Agg')
+import sqlite3
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+import datetime as dt
+import numpy as np
+from matplotlib.ticker import MultipleLocator
+from imf_hist import plot_binned_imf_clock_angle_hist, plot_binned_imf_B_hist
+import sys
+sys.path.append("../")
+from velcomp_vs_time import plot_center_axis
+
+stm = dt.datetime(2011, 1, 1)
+etm = dt.datetime(2017, 1, 1)
+
+dbdir = "../../data/sqlite3/" 
+dbname = "binned_imf.sqlite"
+fig_path = "../plots/gmi_imf/hist_of_imf_binned_by_clock_angle/"
+
+bvec_max = 0.90
+before_mins=50
+after_mins=0
+del_tm=10
+
+# Create clock angle bins
+sector_center_dist = 45
+sector_width = 40
+# set bins for all clock angle ranges
+imf_bins = [[x-sector_width/2, x+sector_width/2] for x in np.arange(0, 360, sector_center_dist)]
+#imf_bins = [[-30, 30] for x in imf_bins]
+# Determines how to place the imf_bins into panels,
+# NOTE: must match with imf_bins
+ax_idxs = [1, 2, 5, 8, 7, 6, 3, 0]
+bins_txt = ["Bz+", "By+/Bz+", "By+", "By+/Bz-",
+	    "Bz-", "By-/Bz-", "By-", "By-/Bz+"]
+
+## set bins for IMF clock angle near 90 or 270
+#sector_centers = [80 - sector_width/2, 100 + sector_width/2,
+#                  260 - sector_width/2, 280 + sector_width/2]
+#imf_bins = []
+#for ctr in sector_centers:
+#    imf_bins.append([ctr - sector_width/2, ctr + sector_width/2])
+##bins_txt = ["Bz+", "By+", "Bz-", "By-"]
+#bins_txt = ["By+, Bz+", "By+, Bz-", "By-, Bz-", "By-, Bz+"]
+
+seasons = "winter"
+kp_lim = [0.0, 2.3]
+#colors=['k', 'b', 'g', 'orange']
+
+def clock_angle_hist(all_in_one_axis=False):
+    """
+    Plots the hist of clock angles,
+    NOTE: one IMF bin per panel if all_in_one_axis is False
+    """
+
+    colors=['k'] * len(imf_bins)
+    xlim=[-45, 360]
+    ylim=[0, 35000]
+    # create subplots
+    if all_in_one_axis:
+	fig, ax = plt.subplots()
+	# plot the histogram of binned imf_clock_angle
+	plot_binned_imf_clock_angle_hist(stm, etm, dbdir, dbname, ax=ax, bvec_max=bvec_max,
+					before_mins=before_mins, after_mins=after_mins,
+					del_tm=del_tm, sector_center_dist=sector_center_dist,
+					bins=imf_bins, colors=colors, kp_lim=kp_lim, alpha=1.0)
+
+        fig_name = "hist_imf_clock_angle_v1_" + \
+                   stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
+                   "_before" + str(before_mins) + "_after" +  str(after_mins) + \
+                   "_bvec" + str(bvec_max).split('.')[-1] + "_kp_" +\
+                   "_to_".join(["".join(str(x).split('.')) for x in kp_lim])
+
+    else:
+	fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(12,6),
+				 sharex=True, sharey=True)
+	axes = [x for subls in axes for x in subls]
+	fig.subplots_adjust(hspace=0.3)
+
+	for i, imf_bin in enumerate(imf_bins):
+	    ax = axes[ax_idxs[i]]
+	    # plot the histogram of binned imf_clock_angle
+	    plot_binned_imf_clock_angle_hist(stm, etm, dbdir, dbname, ax=ax, bvec_max=bvec_max,
+					    before_mins=before_mins, after_mins=after_mins,
+					    del_tm=del_tm, sector_center_dist=sector_center_dist,
+                                            xlim=xlim, ylim=ylim,
+					    bins=[imf_bin], colors=colors, kp_lim=kp_lim, set_labels=False)
+            ax.set_title("Hist. of IMF Clock Angle", fontsize=10)
+
+        # Plot the center axis for IMF clock angle
+        plot_center_axis(axes[4], sector_center_dist=sector_center_dist,
+                         sector_width=sector_width, xlim=xlim, ylim=ylim)
+
+         # Set axis labels
+        # Add label to first column and last row
+        for i in [0, 3, 6]:
+            axes[i].set_ylabel("Count", fontsize=9)
+            axes[i].yaxis.set_major_locator(MultipleLocator(base=10000))
+        for i in range(6,9):
+            axes[i].set_xlabel("IMF Clock Angle [Degree]", fontsize=9)
+            axes[i].xaxis.set_major_locator(MultipleLocator(base=sector_center_dist))
+            # Set tick fontsize
+            for tick in axes[i].xaxis.get_major_ticks():
+                tick.label.set_fontsize(8)
+                tick.label.set_rotation(30)
+
+        # Save the figure
+        fig_name = "hist_imf_clock_angle_v2_" + \
+                   stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
+                   "_before" + str(before_mins) + "_after" +  str(after_mins) + \
+                   "_bvec" + str(bvec_max).split('.')[-1] + "_kp_" +\
+                   "_to_".join(["".join(str(x).split('.')) for x in kp_lim])
+
+    fig.savefig(fig_path+fig_name + ".png", dpi=200, bbox_inches="tight")
+    #fig.savefig(fig_path+fig_name + ".pdf", format="pdf", bbox_inches="tight")
+    plt.close(fig)
+
+    return
+
+
+def IMF_B_hist():
+
+    """
+    Plots the hist of certain IMF component,
+    """
+    param_list = ["By", "Bz"]
+    colors=['k'] * len(imf_bins)
+    xlim=[-20, 20]
+    ylim=[0, 120000]
+
+    for param in param_list:
+        fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(12,6),
+                                 sharex=True, sharey=True)
+        axes = [x for subls in axes for x in subls]
+        fig.subplots_adjust(hspace=0.3)
+
+        for i, imf_bin in enumerate(imf_bins):
+            ax = axes[ax_idxs[i]]
+            # Plot the hist of imf B in a given imf bin
+            plot_binned_imf_B_hist(stm, etm, dbdir, dbname, axes=[ax], bvec_max=bvec_max,
+                                   before_mins=before_mins, after_mins=after_mins,
+                                   del_tm=del_tm, sector_center_dist=sector_center_dist,
+                                   imf_bin=imf_bin, params=[param], colors=colors, xlim=xlim, ylim=ylim,
+                                   kp_lim=kp_lim, set_labels=False)
+            ax.set_title("Hist. of IMF " + param, fontsize=10)
+
+        # Plot the center axis for IMF clock angle
+        plot_center_axis(axes[4], sector_center_dist=sector_center_dist,
+                         sector_width=sector_width, xlim=xlim, ylim=ylim)
+
+         # Set axis labels
+        # Add label to first column and last row
+        for i in [0, 3, 6]:
+            axes[i].set_ylabel("Count", fontsize=9)
+            axes[i].yaxis.set_major_locator(MultipleLocator(base=20000))
+        for i in range(6,9):
+            axes[i].set_xlabel(param + " [nT]", fontsize=9)
+            axes[i].xaxis.set_major_locator(MultipleLocator(base=4))
+            # Set tick fontsize
+            for tick in axes[i].xaxis.get_major_ticks():
+                tick.label.set_fontsize(8)
+                #tick.label.set_rotation(30)
+
+
+        # Save figure
+        fig_name = "binned_imf_" + param + "_hist_" + \
+                   stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
+                   "_before" + str(before_mins) + "_after" +  str(after_mins) + \
+                   "_bvec" + str(bvec_max).split('.')[-1] + "_kp_" +\
+                   "_to_".join(["".join(str(x).split('.')) for x in kp_lim])
+
+        fig.savefig(fig_path+fig_name + ".png", dpi=200, bbox_inches="tight")
+        #fig.savefig(fig_path+fig_name + ".pdf", format="pdf", bbox_inches="tight")
+        plt.close(fig)
+
+    return
+
+if __name__ == "__main__":
+    #clock_angle_hist(all_in_one_axis=False)
+    IMF_B_hist()
--- plotting/gmi_imf/imf_hist.py
+++ plotting/gmi_imf/imf_hist.py
@@ -1,11 +1,11 @@
 import matplotlib
 matplotlib.use('Agg')
-
+import numpy as np
 import sqlite3
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-def plot_imf_hist(stm, etm, dbdir, dbname, param="theta"):
+def plot_imf_hist(stm, etm, dbdir, dbname, ax=None, param="theta"):
 
     """ Plots histograms of 1-min imf from omni for the period between stm and etm. 
     NOTE: No stability condition is inforced here
@@ -15,7 +15,8 @@ def plot_imf_hist(stm, etm, dbdir, dbname, param="theta"):
     conn = sqlite3.connect(dbdir + dbname, detect_types = sqlite3.PARSE_DECLTYPES)
     cur = conn.cursor()
 
-    fig, ax = plt.subplots()
+    if ax is None:
+        fig, ax = plt.subplots()
     
     input_table = "IMF"
     command = "SELECT Bx, By, Bz, theta FROM {tb} " + \
@@ -54,11 +55,12 @@ def plot_imf_hist(stm, etm, dbdir, dbname, param="theta"):
     ax.set_ylim([0, 450000])
 
 
-    return fig
+    return
 
-def plot_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_mins=20, 
+def plot_binned_imf_clock_angle_hist(stm, etm, dbdir, dbname, ax=None, bvec_max=0.95, before_mins=20, 
 			      after_mins=10, del_tm=10, sector_center_dist = 90,
-			      bins=[[-30, 30], [150, 210]],
+			      bins=[[-30, 30], [150, 210]], set_labels=True,
+                              xlim=[-45, 360], ylim=[0, 45000], alpha=0.8,
 			      colors=['k', 'b', 'g', 'orange'], kp_lim=[0.0, 9.0]):
 
     """ Plots histograms of 1-min imf(from omni) clock angles for the given clock angle bins
@@ -68,7 +70,8 @@ def plot_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_min
     conn = sqlite3.connect(dbdir + dbname, detect_types = sqlite3.PARSE_DECLTYPES)
     cur = conn.cursor()
 
-    fig, ax = plt.subplots()
+    if ax is None:
+        fig, ax = plt.subplots()
     
     # Loop through each imf clock angle bin
     for i, bn in enumerate(bins):
@@ -83,13 +86,18 @@ def plot_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_min
 	cur.execute(command)
 	rws = cur.fetchall()
 	rws = [x[0] for x in rws]
-        if bn[0] < 0:
-            rws = [x if x < sector_center_dist else x - 360 for x in rws] 
+
+        # Wrap the tailing points that exceed 0 or 360
+        #if bn[0] < 0:
+        if np.mean(bn) <= sector_center_dist:
+            rws = [x if x < 3*sector_center_dist else x - 360 for x in rws] 
+        if np.mean(bn) >= 360-sector_center_dist:
+            rws = [x if x > np.mean(bn)-2*sector_center_dist else x + 360 for x in rws] 
 
 	# plot the histogram
 	bn_width = bn[1] - bn[0]
 	nbins = (360 + bn_width)/5 - 1 
-	ax.hist(rws, bins=nbins, range=(-bn_width, 360), color=colors[i], alpha=0.8)
+	ax.hist(rws, bins=nbins, range=(-bn_width, 360), color=colors[i], alpha=alpha)
 	#ax.hist(rws, bins=71, range=(-60, 360), color=colors[i], alpha=0.6)
 	
         # plot the boundary of each bins
@@ -98,30 +106,90 @@ def plot_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_min
 	ax.axvline(x=xs[1], color='r', linestyle="-", linewidth=0.5)
 	#ax.axvline(x=xs[2], color=colors[i], linestyle="--", linewidth=0.5)
 
+    ax.set_ylim(ylim)
+    ax.set_xlim(xlim)
     # set lables
-    ax.set_xlabel("IMF Clock Angle [degree]")
-    ax.set_ylabel("Count")
-    ax.set_title("Histogram of IMF Clock Angle under Stable IMF Condition", fontsize="medium")
-    ax.set_xlim([-sector_center_dist, 360])
-    ax.set_ylim([0, 45000])
-    ax.set_xticks(range(-sector_center_dist, 360+sector_center_dist, sector_center_dist))
+    if set_labels:
+        ax.set_xlabel("IMF Clock Angle [degree]")
+        ax.set_ylabel("Count")
+        ax.set_title("Histogram of IMF Clock Angle under Stable IMF Condition", fontsize="medium")
+        ax.set_xticks(range(-sector_center_dist, 360+sector_center_dist, sector_center_dist))
 
-    return fig
+    return
 
-def plot_binned_imf_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_mins=20, 
-			      after_mins=10, del_tm=10, sector_center_dist = 90,
-			      imf_bin=[-30, 30],
-			      colors=['k', 'b', 'g', 'orange'], kp_lim=[0.0, 9.0]):
+def plot_binned_imf_B_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_mins=20, 
+                          after_mins=10, del_tm=10, sector_center_dist = 90,
+                          imf_bin=[-30, 30], axes=None, params=["Bx", "By", "Bz"],
+                          xlim=[-20, 20], ylim=[0, 45000], alpha=0.8, set_labels=True,
+                          colors=['k', 'b', 'g'], kp_lim=[0.0, 9.0]):
 
-    """ Plots histograms of 1-min imf Bs and theta (from omni) for a given clock angle bin
+    """ Plots histograms of 1-min imf Bs (Bx, By, Bz) from omni for a given clock angle bin
     """
 
     # make a db connection
     conn = sqlite3.connect(dbdir + dbname, detect_types = sqlite3.PARSE_DECLTYPES)
     cur = conn.cursor()
 
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8,6))
-    fig.subplots_adjust(hspace=0.5)
+    if axes is None:
+        fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8,6))
+        axes = [ax for sublst in axes for ax in sublst]
+    plt.gcf().subplots_adjust(hspace=0.5)
+    
+    # Loop through each imf clock angle bin
+    input_table = "b" + str((imf_bin[0]%360)) + "_b" + str(imf_bin[1]%360) + \
+                  "_before" + str(before_mins) + "_after" +  str(after_mins) + \
+                  "_bvec" + str(bvec_max).split('.')[-1] + "_all"
+
+    command = "SELECT Bx, By, Bz FROM {tb} " + \
+              "WHERE (datetime BETWEEN '{stm}' AND '{etm}') AND " +\
+              "(kp BETWEEN {kp_low} AND {kp_high})"
+    command = command.format(tb=input_table, stm=stm, etm=etm, 
+                             kp_low=kp_lim[0], kp_high=kp_lim[1])
+    cur.execute(command)
+    rws = cur.fetchall()
+
+    imf_bin_width = imf_bin[1] - imf_bin[0]
+    nbins = [40, 40, 40]
+    rngs = [[-20, 20], [-20, 20], [-20, 20]]
+    xlabels = {"Bx":"Bx [nT]", "By":"By [nT]", "Bz":"Bz [nT]"}
+    titles = params
+
+    rw_i_dct = {"Bx":0, "By":1, "Bz":2}  # The key-value pairs should match with the params order after SELECT
+    for i in range(len(params)):
+        ax = axes[i]
+        var = [x[rw_i_dct[params[i]]] for x in rws]
+
+	# plot the histogram
+	ax.hist(var, bins=nbins[i], range=rngs[i], color=colors[i], alpha=alpha)
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+
+        # set lables
+        if set_labels:
+            ax.set_xlabel(xlabels[params[i]])
+            ax.set_ylabel("Count")
+            ax.set_title("Histogram of IMF " + titles[i], fontsize="medium")
+
+
+    return
+
+
+def plot_binned_imf_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_mins=20, 
+			      after_mins=10, del_tm=10, sector_center_dist = 90,
+			      imf_bin=[-30, 30], axes=None,
+			      colors=['k', 'b', 'g', 'orange'], kp_lim=[0.0, 9.0]):
+
+    """ Plots histograms of 1-min imf Bs (Bx, By, Bz) and
+    theta (from omni) for a given clock angle bin
+    """
+
+    # make a db connection
+    conn = sqlite3.connect(dbdir + dbname, detect_types = sqlite3.PARSE_DECLTYPES)
+    cur = conn.cursor()
+
+    if axes is None:
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8,6))
+    plt.gcf().subplots_adjust(hspace=0.5)
     axes = [ax for sublst in axes for ax in sublst]
     
     # Loop through each imf clock angle bin
@@ -163,47 +231,60 @@ def plot_binned_imf_hist(stm, etm, dbdir, dbname, bvec_max=0.95, before_mins=20,
             ax.set_xticks(range(-sector_center_dist, 360+sector_center_dist, sector_center_dist))
         else:
             ax.set_xlim([-20, 20])
-            ax.set_ylim([0, 45000])
+            ax.set_ylim([0, 60000])
 
-    return fig
-
+    return
 
 if __name__ == "__main__":
 
     import datetime as dt
-    import numpy as np
 
     stm = dt.datetime(2011, 1, 1)
     etm = dt.datetime(2017, 1, 1)
 
-    dbdir = "../../data/sqlite3/"
-    
-    #################################################
-    # Plot the histogram of IMF for stable period
-
+    dbdir = "../../data/sqlite3/" 
     dbname = "binned_imf.sqlite"
-    bvec_max = 0.85
-    before_mins=80
+    fig_path = "../plots/gmi_imf/"
+
+    bvec_max = 0.95
+    before_mins=50
     after_mins=0
     del_tm=10
+
+#    bvec_max = 0.85
+#    before_mins=80
+#    after_mins=0
+#    del_tm=10
+
     kp_lim = [0.0, 2.3]
     #kp_lim = [0.0, 9.0]
     #kp_lim = [2.6, 5.0]
     colors=['k', 'b', 'g', 'orange']
 
-    # set the bins
-    sector_width = 60
-    sector_center_dist = 90
-    bins = [[x-sector_width/2, x+sector_width/2] for x in np.arange(0, 360, sector_center_dist)]
 
-    fig_path = "../plots/gmi_imf/"
-    fig_name = "hist_imf_clock_angle_" + \
-               stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
-               "_before" + str(before_mins) + "_after" +  str(after_mins) + \
-               "_bvec" + str(bvec_max).split('.')[-1] + "_kp_" +\
-               "_to_".join(["".join(str(x).split('.')) for x in kp_lim]) + ".png"
+    sector_center_dist = 90
+    # set bins for all clock angle range
+#    sector_width = 60
+#    bins = [[x-sector_width/2, x+sector_width/2] for x in np.arange(0, 360, sector_center_dist)]
+    # set bins for IMF clock angle near 90 or 270
+    sector_width = 30 
+    sector_centers = [80 - sector_width/2, 100 + sector_width/2, 
+                      260 - sector_width/2, 280 + sector_width/2]
+    bins = []
+    for ctr in sector_centers:
+        bins.append([ctr - sector_width/2, ctr + sector_width/2])
+
+    #bins = [[50, 80]]
+
+    #################################################
+#    # Plot the histogram of IMF for stable period
+#    fig_name = "hist_imf_clock_angle_" + \
+#               stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
+#               "_before" + str(before_mins) + "_after" +  str(after_mins) + \
+#               "_bvec" + str(bvec_max).split('.')[-1] + "_kp_" +\
+#               "_to_".join(["".join(str(x).split('.')) for x in kp_lim]) + ".png"
 #    # plot the histogram of binned imf_clock_angle
-#    fig = plot_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=bvec_max,
+#    fig = plot_binned_imf_clock_angle_hist(stm, etm, dbdir, dbname, bvec_max=bvec_max,
 #				    before_mins=before_mins, after_mins=after_mins,
 #				    del_tm=del_tm, sector_center_dist=sector_center_dist,
 #                                    bins=bins, colors=colors, kp_lim=kp_lim)
@@ -211,25 +292,7 @@ if __name__ == "__main__":
 
 
     #################################################
-    dbname = "binned_imf.sqlite"
-    bvec_max = 0.95
-    before_mins=50
-    after_mins=0
-    del_tm=10
-    kp_lim = [0.0, 2.3]
-    #kp_lim = [0.0, 9.0]
-    #kp_lim = [2.6, 5.0]
-    colors=['k', 'b', 'g', 'orange']
-
-    # set the bins
-    sector_width = 60
-    sector_center_dist = 90
-    bins = [[x-sector_width/2, x+sector_width/2] for x in np.arange(0, 360, sector_center_dist)]
-    #bins = [[-30, 30]]
-
     # Plot the hist of imf params in a given imf bin
-
-    fig_path = "../plots/gmi_imf/"
     for bn in bins:
         fig_name = "binned_imf_hist_" + \
                    stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") +\
@@ -250,7 +313,6 @@ if __name__ == "__main__":
 #    dbname = "gmi_imf.sqlite"
 #    #param = "theta"
 #    param = "Bx"
-#    fig_path = "../plots/gmi_imf/"
 #    fig_name = "hist_all_imf_" + param + "_" + \
 #               stm.strftime("%Y%m%d") + "_" + etm.strftime("%Y%m%d") + ".png"
 #    fig = plot_imf_hist(stm, etm, dbdir, dbname, param=param)
